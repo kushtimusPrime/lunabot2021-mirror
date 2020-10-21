3c8f3f07b76f53120f852745f533138bfb4f8d50
kushtimusPrime <kush.j.hari@vanderbilt.edu>
2020-10-21 00:54:24 +0000

    Fixed velocity conversion, but no scaling yet

--- teleop_a/src/Velocity_Conversion.py
+++ teleop_a/src/Velocity_Conversion.py
@@ -15,64 +15,39 @@ class Velocity:
 
         	self.left_speed = rospy.Subscriber("cmd_vel",Twist,self.method)
 
+    def method(self,data):
     
-def speeds_from_twist(self,data):
+        # get data from twist
         linear = data.linear.x
         angular = data.angular.z
-        r = float(0.1425)
-
-
-        theta = angular*250
-        r = linear*(100/0.340)
-
-        v_a = r*(45-theta%90)/45
-        v_b = min(100,abs(2*r+v_a),abs(2*r-v_a))
-
-        if(theta<-90):
-            v_b=-v_b
-            v_a=-v_a
-        
-        if(theta<0):
-            v_a=-v_a
-
-    self.publishers["pub_left_control"].publish(v_a)
-    self.publishers["pub_right_control"].publish(v_b)
-
-    if theta<-90: return -v_b,-v_a
-    if theta<0: return -v_a,v_b
-    if theta<90: return v_b,v_a
-
-
-
-def method(self,data):
-    
-    # get data from twist
-    linear = data.linear.x
-    angular = data.angular.z
 
     # fixed separation distance from wheels
-    r = float(0.1425)
+        r = float(1)
 
     # gets a and b numbers using r, linear, angular
-    a = float(linear - r * angular)
-    b = float(linear + r * angular)
+        a = float(linear - r * angular)
+        b = float(linear + r * angular)
 
     # gets linear velocity for each side of robot (a = left side) (b = right side)
-    a_Percentage = float(a / linear)
-    b_Percentage = float(b / linear)
+        a_Percentage = float(a / 0.34)
+        b_Percentage = float(b / 0.34)
 
     # scales each side to the maximum linear velocity
-    if(a_Percentage > b_Percentage):
-        a = 1
-        b = b_Percentage / a_Percentage
-    else:
-        b = 1
-        a = a_Percentage / b_Percentage
+        if(abs(a_Percentage)>1 or abs(b_Percentage)>1):
+            if(abs(a_Percentage) > abs(b_Percentage)):
+                a = 100*(a_Percentage / abs(a_Percentage))
+                #Do whatever
+                b = 100*(b_Percentage / abs(a_Percentage))
+            else:
+                b = 100*(b_Percentage / abs(b_Percentage))
+                a = 100*(a_Percentage / abs(b_Percentage))
+        else:
+            a=a_Percentage*100
+            b=b_Percentage*100
     
-    
-    self.publishers["pub_left_control"].publish(a)
-    self.publishers["pub_right_control"].publish(b)
-    return a, b
+        self.publishers["pub_left_control"].publish(a)
+        self.publishers["pub_right_control"].publish(b)
+        return a, b
 
 
 
